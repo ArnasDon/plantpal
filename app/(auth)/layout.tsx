ab3ab44556cdf58847b6aa3c7398601b5331ceed
Cienias <cieja0@gmail.com>
2025-08-29 13:43:09 +0200

    fix: sign-in layout and style

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="auth-layout bg-[url('/images/img-bg.png')] bg-cover bg-center bg-no-repeat">
-      {children}
+    <div className="bg-[url('/images/img-bg.png')] bg-cover bg-center bg-no-repeat">
+      <div className="auth-layout bg-black/40">{children}</div>
     </div>
   );
 };
--- components/SignInCard.tsx
+++ components/SignInCard.tsx
@@ -12,7 +12,7 @@ const SignInCard = () => {
   };
 
   return (
-    <div className="bg-dark-200 shadow-card flex max-w-[500px] flex-col items-center justify-center gap-8 rounded-xl px-8 py-10">
+    <div className="auth-card">
       <div className="mb-2 flex items-center gap-2">
         <Image
           src="/icons/logo.svg"
@@ -23,15 +23,14 @@ const SignInCard = () => {
         />
         <h2>PlantPal</h2>
       </div>
-      <h2>Start Caring Smarter 🌱</h2>
-      <p className="text-light-200 font-onest text-center text-lg">
-        Your plants will never miss a drink again. Sign in to start tracking
-        care and watering schedules.
-      </p>
-      <button
-        onClick={handleSignIn}
-        className="bg-primary text-light-100 shadow-button hover:bg-primary/80 flex cursor-pointer items-center gap-2 rounded-md px-4 py-2 transition-all duration-300"
-      >
+      <div className="auth-card-title">
+        <h2>Start Caring Smarter 🌱</h2>
+        <p className="auth-card-text">
+          Your plants will never miss a drink again. Sign in to start tracking
+          care and watering schedules.
+        </p>
+      </div>
+      <button onClick={handleSignIn} className="auth-card-button">
         <Image
           src="/icons/google.svg"
           alt="google"
